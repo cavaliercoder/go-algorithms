3e7f876c8d374a0526ab8df6b9c8c63b79599ae4
Ryan Armstrong <ryan@cavaliercoder.com>
2016-07-27 17:32:46 +0800

    Added godoc header

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+This project contains implementations of common computer science related
+algorithms written in Go. My aim is not to provide reference implementations,
+but simply to practice while working my way through the third edition of
+Introduction to Algorithms* from MIT Press.
+
+*/
+package algorithms
